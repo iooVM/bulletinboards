2c68e6973f832dfb6d8991a2ba243c3d2b5adfef
iooVM <mihail@shirshakov.ru>
2021-09-02 12:54:40 +0300

    123

---
+++ SkillFactoryD3.4/NewsPortal/news/templatetags/custom_filters.py
@@ -0,0 +1,4 @@
+from django import template
+
+register = template.Library()  # если мы не зарегистрируем наши фильтры, то Django никогда не узнает, где именно их искать и фильтры потеряются
+
--- SkillFactoryD3.4/NewsPortal/news/views.py
+++ SkillFactoryD3.4/NewsPortal/news/views.py
@@ -17,9 +17,11 @@ class AuthorDetail(DetailView):
     context_object_name = 'author'  # название объекта. в нём будет
 
 class PostList(ListView):
-    model = Post  # указываем модель, объекты которой мы будем выводить
+    model = Post # указываем модель, объекты которой мы будем выводить
+    # model.order_by('-dateCreation')
     template_name = 'posts.html'  # указываем имя шаблона, в котором будет лежать HTML, в котором будут все инструкции о том, как именно пользователю должны вывестись наши объекты
     context_object_name = 'posts'  # это имя списка, в котором будут лежать все объекты, его надо указать, чтобы обратиться к самому списку объектов через HTML-шаблон
+    queryset = Post.objects.order_by('-dateCreation') # Сортировка по дате создания
 
 # создаём представление, в котором будут детали конкретного отдельного товара
 class PostDetail(DetailView):
