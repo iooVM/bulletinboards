9415b81e1859a22906c2f938d6f0e5e29ee45d14
iooVM <mihail@shirshakov.ru>
2021-09-13 17:24:53 +0300

    123

--- SkillFactoryD4/NewsPortal/news/filters.py
+++ SkillFactoryD4/NewsPortal/news/filters.py
@@ -6,4 +6,16 @@ from .models import Post
 class PostFilter(FilterSet):
     class Meta:
         model = Post
-        fields = ('author', 'categoryType', 'dateCreation', 'postCategory', 'title', 'content','rating' )  # поля, которые мы будем фильтровать (т. е. отбирать по каким-то критериям, имена берутся из моделей)
+
+#        fields = ('author', 'categoryType', 'dateCreation', 'postCategory', 'title', 'content','rating' )  # поля, которые мы будем фильтровать (т. е. отбирать по каким-то критериям, имена берутся из моделей)
+#          fields = {
+# #             'dateCreation':  ['gt'],
+#              'author': ['icontains'],
+#              'title':  ['icontains'],
+#         }
+        fields = {
+             'title':['icontains'],
+             'author__authorUser__username': ['icontains'],
+             'dateCreation': ['gt'],
+
+        }
